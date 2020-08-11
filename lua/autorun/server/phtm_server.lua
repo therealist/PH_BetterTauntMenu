a4815bfa1434edc9205bb63ccfe0233e5c195412
The Realist <schtherealist@hotmail.com>
2020-08-11 13:40:40 -0500

    Update phtm_server.lua

--- lua/autorun/server/phtm_server.lua
+++ lua/autorun/server/phtm_server.lua
@@ -1,18 +1,19 @@
 if ( SERVER )	then 
-	TAUNT_DELAY = 6		//Taunt Delay Variable
 	util.AddNetworkString("send_taunt")
 	net.Receive( "send_taunt", function( _, ply )
 		local taunt = net.ReadString()
+		local length = net.ReadFloat()
 		local curtime = CurTime()
-		if CurTime() > ply:GetNWFloat("nexttaunt") then
+		if curtime > ply:GetNWFloat("nexttaunt") then
 			if ply:Alive() then
 				local timestaunted = ply:GetNWInt("timestaunted")
 				timestaunted = timestaunted + 1
 				ply:EmitSound(taunt,150)
-				ply:SetNWFloat("lasttaunt", CurTime())
-				ply:SetNWFloat("nexttaunt", CurTime() + TAUNT_DELAY)
+				ply:SetNWFloat("lasttaunt", curtime)
+				ply:SetNWFloat("nexttaunt", curtime + length)
+				ply:SetNWFloat("tauntlength", length)
 				ply:SetNWInt("timestaunted", timestaunted)
-				ply.nexttaunt = CurTime() + TAUNT_DELAY
+				ply.nexttaunt = curtime + length
 			end
 		else
 			ply:ChatPrint("Please wait before taunting again")

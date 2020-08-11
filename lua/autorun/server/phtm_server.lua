74b8a25b7bfa29e82aba8a313b488af87e352a30
The Realist <schtherealist@hotmail.com>
2020-08-11 13:53:01 -0500

    Update phtm_server.lua

--- lua/autorun/server/phtm_server.lua
+++ lua/autorun/server/phtm_server.lua
@@ -1,3 +1,18 @@
+--//-------------------------------------------------------------------
+--//
+--//	DISTRO: 		Better Taunt Menu
+--//	AUTHORS:		Vash Baldeus, Sean Hayes
+--//	BASE:			AMT Prop Hunt
+--//	BUILD:			v2.0
+--//	RELEASE:		08.11.2020
+--//	LICENSED TO:	GNU Public v3.0
+--//				
+--//	> phtm_server.lua
+--//	This file contains the server-side code for the
+--//	Prop Hunt taunt menu.
+--//
+--//-------------------------------------------------------------------
+
 if ( SERVER )	then 
 	util.AddNetworkString("send_taunt")
 	net.Receive( "send_taunt", function( _, ply )
